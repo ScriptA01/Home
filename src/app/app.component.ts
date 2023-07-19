91036d9b7650e9b4a809b8fceb8a0db8041909b2
Your Name <you@example.com>
2023-07-19 17:52:54 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HomeComponent,
+    RouterModule,
+  ],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
+
+
+/*Neste ponto, você definiu uma interface que representa dados sobre um local de habitação, incluindo um id, nome e informações de localização. */
+
+/*Componente principal da nossa aplicação */
